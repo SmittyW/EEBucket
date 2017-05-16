6689a1957885e9914140f30adc515c8fa2c5e962
SmittyW <raspberryicet@gmail.com>
2017-05-15 20:36:22 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -69,6 +69,9 @@ function connect() {
 							runTool();
 						}
 					}
+					else if (m.type == "reset") {
+						BH.message(m, "serialised");	
+					}
 				});
 			}, callbackError);
 		}, callbackError);
